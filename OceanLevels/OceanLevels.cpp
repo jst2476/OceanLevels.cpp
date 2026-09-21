302e4c2a2fd86a9bab3d46b377ae6a7c4141fe5f
Javier Toscano <jst2476@email.vccs.edu>
2026-09-21 18:11:28 -0400

    working backwards,  this was my original code before realizing missing semi colons, also practicing saving changes and adding commits and pushing them, still dont quite understand it.

--- OceanLevels/OceanLevels.cpp
+++ OceanLevels/OceanLevels.cpp
@@ -5,15 +5,15 @@ int main()
 {
 	cout << "Total ocean level Rise!" << endl;
 	// Constants
-	const double yearlyRiseRate = 1.5;
-	double increaseAfter5years = yearlyRiseRate * 5;
-	double increaseAfter7years = yearlyRiseRate * 7;
-	double increaseAfter10years = yearlyRiseRate * 10;
-	cout << "Ocean level rise after 5 years: " << increaseAfter5years;
+	const double yearlyRiseRate = 1.5
+	double increaseAfter5years = yearlyRiseRate * 5
+	double increaseAfter7years = yearlyRiseRate * 7
+	double increaseAfter10years = yearlyRiseRate * 10
+	cout << "Ocean level rise after 5 years: " << increaseAfter5years
 		cout << " mm" << endl;
-		cout << "Ocean level rise after 7 years: " << increaseAfter7years;
+		cout << "Ocean level rise after 7 years: " << increaseAfter7years
 		cout << " mm" << endl;
-		cout << "Ocean level rise after 7 years: " << increaseAfter10years;
+		cout << "Ocean level rise after 7 years: " << increaseAfter10years
 		cout << " mm" << endl;
 	return 0;
 }
