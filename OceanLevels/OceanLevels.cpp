f539e47122fcc530a9ed170633840eaac6bc442d
Javier Toscano <jst2476@email.vccs.edu>
2026-09-21 17:52:58 -0400

    Add project files.

---
+++ OceanLevels/OceanLevels.cpp
@@ -0,0 +1,19 @@
+// This program caculates the how much the ocean level has risen over several years
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Total ocean level Rise!" << endl;
+	// Constants
+	const double yearlyRiseRate = 1.5;
+	double increaseAfter5years = yearlyRiseRate * 5;
+	double increaseAfter7years = yearlyRiseRate * 7;
+	double increaseAfter10years = yearlyRiseRate * 10;
+	cout << "Ocean level rise after 5 years: " << increaseAfter5years;
+		cout << " mm!" << endl;
+		cout << "Ocean level rise after 7 years: " << increaseAfter7years;
+		cout << " mm!" << endl;
+		cout << "Ocean level rise after 7 years: " << increaseAfter10years;
+		cout << " mm!" << endl;
+	return 0;
+}
